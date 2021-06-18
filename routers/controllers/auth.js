492c0567cb61192cc31fd26ce611ab6d1b44dfb6
maisaa <maisaa.alkhedr@gmail.com>
2021-06-18 20:14:50 +0200

    login with MySQL

--- routers/controllers/auth.js
+++ routers/controllers/auth.js
@@ -1,19 +1,44 @@
-const usersModel = require('./../../db/models/users');
+// const usersModel = require('./../../db/models/users');
+
+const db = require('../../db/db.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const login = (req, res) => {
-	const { email, password } = req.body;
+	try {
+		const { email, password } = req.body;
 
-	usersModel
-		.authenticateBasic(email, password)
-		.then((result) => {
-			if (result[1] === 200)
-				return res.status(result[1]).json({ token: result[0] });
+		const command = `SELECT * FROM users WHERE email = ?;`;
+		const arr = [email, password];
 
-			res.status(result[1]).json(result[0]);
+		db.query(command, arr, async (err, result) => {
+			if (err) {
+				console.log("Err Login.....:", err);
+			} else if (result.length == 0) {
+				res.status(404).json("This email doesn't exist")
+			} else {
+				const valid = await bcrypt.compare(password, result[0].password);
+				if (valid) {
+					const payload = {
+						userId: result[0]._id,
+						country: result[0].country,
+						role: result[0].role,
+					};
+					const options = {
+						expiresIn: '60m',
+					};
+
+					let token = jwt.sign(payload, process.env.SECRET, options);
+					console.log("token..... ",token);
+					res.status(200).json(token);
+				}
+
+				res.status(403).json('The password you’ve entered is incorrect') ;
+			}
 		})
-		.catch((err) => {
-			res.send(err);
-		});
+	} catch (error) {
+        throw new Error(error.message);
+	}
 };
 
 module.exports = {
